ed2535694622078593eabce582c194a7101fff1d
FRANCO1357 <fmelani13@gmail.com>
2022-09-01 16:46:11 +0200

    array filter

--- api/disc/index.php
+++ api/disc/index.php
@@ -45,6 +45,19 @@
         ]
     ];
 
+    $results = $discs;
+
+    $search = $_GET['search'] ?? '';
+
+    if($search){
+        $results = [];
+        foreach($discs as $disc){
+            $title = strtolower($disc['title']);
+            $searched_term = strtolower($search);
+            if(strpos($title, $searched_term) !== false) $results[] = $disc;
+        }
+    }
+
     header('Content-Type: application/json');
 
-    echo json_encode($discs);
+    echo json_encode($results); 
