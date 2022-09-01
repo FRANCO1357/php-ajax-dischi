d79cd5eab078379b7f0049ecb357616291a53960
FRANCO1357 <fmelani13@gmail.com>
2022-09-01 15:39:53 +0200

    display php discs

---
+++ php/index.php
@@ -0,0 +1,29 @@
+<?php
+
+include 'data.php';
+
+echo $discs;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Ajax Dischi</title>
+</head>
+<body>
+
+    <h1>Dischi</h1>
+    <?php foreach($discs as $key => $disc) : ?>
+        <h3><?php echo $disc['title'] ?></h3>
+        <p><?php echo $disc['author'] ?></p>
+        <p><?php echo $disc['year'] ?></p>
+        <img src="<?php echo $disc['poster'] ?>" alt="<?php echo $disc['title'] ?>">
+        <p><?php echo $disc['genre'] ?></p>
+    <?php endforeach ?>
+    
+</body>
+</html>
