568fb97e703141923484edf8c2bc7e6ddee21e05
FRANCO1357 <fmelani13@gmail.com>
2022-09-08 15:13:22 +0200

    css

--- vue/script.js
+++ vue/script.js
@@ -4,7 +4,9 @@ Vue.config.devtools = true;
 const app = new Vue({
     el:  '#root',
     data: {
-        discs: []
+        discs: [],
+        search: '',
+        genre: '',
     },
     mounted(){
         axios.get('../api/disc/index.php').then(res => {
